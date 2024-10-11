c4ae0e637a68805764d1c390e65cefa604969349
aidenseibel <aiden.seibel@gmail.com>
2024-10-11 13:24:13 -0500

    styling and bugfixes

--- projects.js
+++ projects.js
@@ -2,62 +2,68 @@
 const ios_projects = [
     {
         name: "CarbonSaurus", 
-        description: "CarbonSaurus is an iOS app that uses a virtual pet dinosaur to keep you climate aware.", 
-        image_url: "/images/carbonsaurus_image.jpg"
-    },
-    {
-        name: "Shrdded", 
-        description: "Shrdded is an iOS app that empowers users in their local weightlifting communities via posting personal records, interacting with other lifters, and participating in competitions.", 
-        image_url: "/images/shrdded.png"
+        description: "RowdyHacks 2024 Winner. A virtual pet dinosaur keeps users climate aware using a carbon diary, avatar customization, daily quizzes and lessons, and more.", 
+        image_url: "./images/carbonsaurus_image.jpg"
     },
     {
         name: "Freelance (Fiverr)", 
-        description: "Did freelance iOS development for 8 different projects and maintained a five star rating.", 
-        image_url: "/images/fiverr.jpg"
+        description: "Private contracts ranging from extensive apps to small bugfixing. Maintained a five star rating across 12 projects in six months.", 
+        image_url: "./images/fiverr.jpg"
     },
     {
         name: "Traverse", 
-        description: "Traverse is an iOS peer-to-peer marketplace for renting appliances, equipment, electronics, and more.", 
-        image_url: "/images/traverse.jpg"
+        description: "A peer-to-peer marketplace for renting appliances, equipment, electronics, and more.", 
+        image_url: "./images/traverse.jpg"
+    },
+    {
+        name: "Shrdded", 
+        description: "Local weightlifting communities can share their personal records, interact with other lifters, and participate in competitions.", 
+        image_url: "./images/shrdded.png"
     },
     {
         name: "Studia", 
-        description: "Studia is an iOS app that utilizes generative AI to create study materials for students.", 
-        image_url: "/images/studia.jpeg"
+        description: "Generative AI and OCR helps students create study materials for their university classes.", 
+        image_url: "./images/studia.jpeg"
     },
     {
         name: "Combatibles", 
-        description: "Combatible is an iOS party game app that lets two pairs of users compete on their trivia knowledge.", 
-        image_url: "/images/combatibles.jpg"
-    }
+        description: "Friends and family test their knowledge of each other in a fast-paced trivia game.", 
+        image_url: "./images/combatibles.jpg"
+    },
 ];
 
 // Deep Learning Projects
 const deep_learning_projects = [
     {
-        name: "TxDOT ML", 
-        description: "Built multimodal neural networks to classify 19 fields in vehicle crash reports, and integrated models into TxDOT's information system.", 
-        image_url: "/images/txdot_interpreted_fields.png"
+        name: "Interpreted Fields", 
+        description: "Built multimodal neural networks (Tensorflow/Keras) for classification within TxDOT's information system. Saved $900,000 USD per year from private contractor.", 
+        image_url: "./images/txdot_interpreted_fields.png"
     }
 ];
 
 // Chess Programming Projects
 const chess_programming_projects = [
     {
-        name: "The Haskell Engine", 
-        description: "A purely functional, 1500-strength chess engine written in Haskell.", 
-        image_url: "/images/haskell_chess_engine.jpg"
+        name: "The Python Engine", 
+        description: "An 1800-strength Python engine. Utilized convolutional neural networks to approximate an evaluation function.", 
+        image_url: "./images/python_chess_engine.jpg"
     },
     {
         name: "The Java Engine", 
         description: "A purely object-oriented, 2000-strength chess engine written in Java.", 
-        image_url: "/images/java_chess_engine.jpg"
+        image_url: "./images/java_chess_engine.jpg"
+    },
+    {
+        name: "The Haskell Engine", 
+        description: "A purely functional, 1500-strength chess engine written in Haskell.", 
+        image_url: "./images/haskell_chess_engine.jpg"
     },
     {
         name: "The Julia Engine", 
         description: "A 1300-strength chess engine written in Julia.", 
-        image_url: "/images/julia_chess_engine.jpg"
-    }
+        image_url: "./images/julia_chess_engine.jpg"
+    },
+
 ];
 
 // Web Development Projects
@@ -65,22 +71,27 @@ const web_development_projects = [
     {
         name: "hamsterstore", 
         description: "An online marketplace for freelance hamster food vendors.", 
-        image_url: "/images/hamsterstore.jpg"
+        image_url: "./images/hamsterstore.jpg"
     },
     {
         name: "Personal Website", 
-        description: "Well - it's this website, written entirely using ExpressJS and Node.", 
-        image_url: "/images/pfp.jpg"
+        description: "Well - it's this website. Converted from ExpressJS / Node", 
+        image_url: "./images/pfp.jpg"
     }
 ];
 
 // Function to create project cards
 function createProjectCard(project) {
     return `
-        <div class="project_card">
-            <img src="${project.image_url}" alt="${project.name}" />
-            <h3>${project.name}</h3>
-            <p>${project.description}</p>
+        <div class="project_card_container">
+            <div class="project_card_image_container">
+                <img src="${project.image_url}" alt="${project.name}" />
+            </div>
+
+            <div class="project_card_text_container">
+                <h2>${project.name}</h2>
+                <p style="overflow: hidden; text-overflow: ellipsis;">${project.description}</p>
+            </div>
         </div>
     `;
 }
